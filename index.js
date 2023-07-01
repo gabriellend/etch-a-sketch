be8384425fe31bd6fc2736be48d57838ba93f9a3
gabriellend <gdvdsn@gmail.com>
2023-06-30 19:34:29 -1000

    Add darken function

--- index.js
+++ index.js
@@ -9,15 +9,41 @@ const colors = document.querySelectorAll(".color");
 let currentColor = "";
 
 // FUNCTIONS
+const darken = (color) => {
+  // color comes in as an rgb string. This isolates just the values.
+  const rgbValues = color.slice(4, color.length - 1).split(", ");
+  const tenPercentOfInitialRGBValue = 255 * 0.1;
+
+  let darkenedColorRGBValues = [];
+  for (let i = 0; i < rgbValues.length; i++) {
+    if (Number(rgbValues[i]) > 0) {
+      const newValue = Number(rgbValues[i]) - tenPercentOfInitialRGBValue;
+      darkenedColorRGBValues.push(newValue);
+    } else {
+      darkenedColorRGBValues.push(rgbValues[i]);
+    }
+  }
+
+  const darkenedColor = `rgb(${darkenedColorRGBValues[0]}, ${darkenedColorRGBValues[1]}, ${darkenedColorRGBValues[2]})`;
+  currentColor = darkenedColor;
+
+  return darkenedColor;
+};
+
 const chooseColor = (e) => {
   const computedStyle = window.getComputedStyle(e.target);
   const backgroundColor = computedStyle.backgroundColor;
-
   currentColor = backgroundColor;
 };
 
 const draw = (e) => {
-  if (e.buttons === 1 || e.type === "click") {
+  if (
+    (e.buttons !== 1 && e.type === "click") ||
+    (e.buttons === 1 && e.type === "mousedown")
+  ) {
+    const newColor = darken(currentColor);
+    e.target.style.backgroundColor = newColor;
+  } else if (e.buttons === 1 || e.type === "click") {
     e.target.style.backgroundColor = currentColor;
   }
 };
@@ -63,7 +89,6 @@ const clearSketchPad = () => {
   sketchPad.innerHTML = "";
 
   clearInput();
-
   button.innerText = "Create";
   button.removeEventListener("click", clearSketchPad);
   button.addEventListener("click", createSketchPad);
@@ -92,6 +117,7 @@ const createSketchPad = () => {
       square.classList.add("square");
       square.addEventListener("click", draw);
       square.addEventListener("mousemove", draw);
+      square.addEventListener("mousedown", draw);
 
       column.appendChild(square);
     }
